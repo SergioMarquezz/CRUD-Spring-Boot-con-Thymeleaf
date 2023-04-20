88173c26ace08db70b429d6ecc54b3c57834129a
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-04-19 23:20:10 -0600

    Se agrega puerto de servidor y metodo para mostrar todos los estudiantes

--- src/main/java/com/crud/web/controller/EstudianteController.java
+++ src/main/java/com/crud/web/controller/EstudianteController.java
@@ -1,7 +1,20 @@
 package com.crud.web.controller;
 
+import com.crud.web.service.EstudianteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EstudianteController {
+
+    @Autowired
+    private EstudianteService estudianteImpl;
+
+    @GetMapping({"/estudiantes","/"})
+    public String listarEstudiantes(Model modelo){
+        modelo.addAttribute("estudiantes",estudianteImpl.listarTodosEstudiantes());
+        return "estudiantes";
+    }
 }
