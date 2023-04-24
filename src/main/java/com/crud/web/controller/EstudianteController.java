4c4dba984d04bd2264beab07452dcd1e109b730c
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-04-23 19:13:23 -0600

    Se agrega vista para agregar un nuevo estudiante

--- src/main/java/com/crud/web/controller/EstudianteController.java
+++ src/main/java/com/crud/web/controller/EstudianteController.java
@@ -1,5 +1,6 @@
 package com.crud.web.controller;
 
+import com.crud.web.entity.Estudiante;
 import com.crud.web.service.EstudianteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,5 +17,13 @@ public class EstudianteController {
     public String listarEstudiantes(Model modelo){
         modelo.addAttribute("estudiantes",estudianteImpl.listarTodosEstudiantes());
         return "estudiantes";
+    }
+
+    @GetMapping("/estudiantes/agregar")
+    public String agregarEstudiante(Model model){
+        Estudiante alumno = new Estudiante();
+        model.addAttribute("estudiante",alumno);
+
+        return "agregarEstudiante";
     }
 }
